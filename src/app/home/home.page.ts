4835fa11ba39aedbc0afc856f8410cb2445c3f77
javier <alum.javier.garcia.rosado@ieslosremedios.org>
2020-12-03 10:58:29 +0100

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  users: any;
+  filtro: string='';
 
+  constructor(private httpClient: HttpClient) {
+    this.users = this.httpClient.get('https://raw.githubusercontent.com/memorycolors/responsive/main/src/data/MOCK_DATA%20.json');
+  }
+
+  
 }
